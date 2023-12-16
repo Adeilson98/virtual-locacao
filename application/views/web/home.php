068edcda0e11b7e94ad6c8beff0ef99a343541bf
Adeilson Silva <adeilson.129@gmail.com>
2023-12-16 20:09:59 -0300

    ok

--- application/views/web/home.php
+++ application/views/web/home.php
@@ -32,7 +32,41 @@
     </div>
 
     <div class="about-section">
+
+        <div class="flex-text">
+
+            <h2>Conheça a nossa História</h2>
+
+            <p>
+            Desde 1997, a Virtual tem sido líder incontestável em soluções integradas no universo da impressão, cópia e digitalização. Nossa jornada é pautada pela inovação constante e pela busca incansável por proporcionar aos nossos clientes uma experiência única e eficiente.
+            <br><br>
+            Ao longo dos anos, aprimoramos nossa expertise, adaptando-nos às mudanças tecnológicas e às crescentes demandas empresariais. Investimos em equipamentos de última geração para assegurar que sua empresa alcance níveis inigualáveis de eficiência, agilidade e controle nos processos de impressão.
+            </p>
+
+        </div>
+
+        <img src="<?php echo base_url('assets/site/images/foto1.webp'); ?>">
         
+    </div>
+
+    <div class="commitment-section">
+
+        <img src="<?php echo base_url('assets/site/images/foto2.webp'); ?>">
+
+        <div class="flex-text">
+            <h2>Nosso compromisso:</h2>
+
+            <ul>
+                <li><b>Eficiência:</b> Com a utilização de equipamentos de última geração, garantimos que cada processo de impressão seja executado com máxima eficiência. Desde a simples cópia de um documento até a digitalização de arquivos em rede, estamos comprometidos em otimizar cada etapa do seu fluxo de trabalho.</li>
+
+                <li><b>Agilidade e Controle:</b> Entendemos a importância do tempo no ambiente empresarial. Nossas soluções são projetadas para oferecer agilidade sem sacrificar o controle. Isso significa que, ao escolher a Virtual, você estará no comando de seus processos de impressão, podendo gerenciar, monitorar e adaptar-se conforme necessário.</li>
+
+                <li><b>Customização para Seu Setor:</b> Reconhecemos que cada setor possui necessidades específicas. Nossas soluções completas embarcadas nos equipamentos são flexíveis e customizáveis, permitindo adaptações precisas para atender aos requisitos exclusivos de cada setor da sua empresa.</li>
+
+                <li><b>Suporte Excepcional:</b> Não fornecemos apenas produtos; fornecemos parcerias duradouras. Nossa equipe especializada está pronta para oferecer suporte técnico contínuo, garantindo que sua experiência com nossas soluções seja sempre satisfatória.</li>
+            </ul>
+        </div>
+
     </div>
 
 </section>
