6ad6f58373cab57be4f93e5a99944db4c521d57b
Adeilson Silva <adeilson.129@gmail.com>
2023-12-22 21:35:29 -0300

    ok

--- application/views/web/layout/footer.php
+++ application/views/web/layout/footer.php
@@ -30,6 +30,49 @@
         </div>
 
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3665.812662291635!2d-47.32076602475116!3d-23.249903250223525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf504e9fb1db81%3A0x7eb138b83f985b17!2sR.%20Vit%C3%B3rio%20Fernando%20Leis%2C%20447%20-%20Vila%20Mariah%2C%20Itu%20-%20SP%2C%2013313-180!5e0!3m2!1spt-BR!2sbr!4v1702509171960!5m2!1spt-BR!2sbr" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+        <div class="footer">
+
+            <div class="coluna1">
+
+                <img src="<?php echo base_url('assets/site/images/logo-footer.png'); ?>">
+
+            </div>
+
+            <div class="coluna2">
+
+                <h4>Contatos</h4>
+
+                <div class="flex-hours">
+
+                    <h6>Whatsapp</h6>
+                    <a href="">00 0 0000-0000</a>
+
+                    <h6>Whatsapp</h6>
+                    <a href="">00 0 0000-0000</a>
+
+                    <h6>E-mail</h6>
+                    <a href="">empresa@dominio.com.br</a>
+
+                </div>
+
+            </div>
+
+            <div class="coluna3">
+                <h4>Nos acompanhe em nossas redes sociais!</h4>
+
+                <div class="social-medias">
+
+                    <a href=""><img src="<?php echo base_url('assets/site/images/instagram.png'); ?>" alt="Instagram"></a>
+
+                    <a href=""><img src="<?php echo base_url('assets/site/images/facebook.png'); ?>" alt="Facebook"></a>
+
+                </div>
+                
+            </div>
+
+        </div>
+
     </footer>
     <!-- Scripts -->
 
