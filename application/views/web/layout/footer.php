a87fc4ededdb3cec44d57cc52292d2fd24065b0f
Adeilson Silva <adeilson.129@gmail.com>
2023-12-22 23:01:19 -0300

    smooth scrol made

--- application/views/web/layout/footer.php
+++ application/views/web/layout/footer.php
@@ -75,9 +75,8 @@
 
     </footer>
     <!-- Scripts -->
-
-    <script src="<?php echo base_url('assets/site/js/slider.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/site/js/banner.js'); ?>"></script>
+    
+    <script src="<?php echo base_url('assets/site/js/smooth-scroll.js'); ?>"></script>
 
 </body>
 </html>
